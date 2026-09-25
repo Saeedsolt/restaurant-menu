029bf742a3e72aa89ce5340b5135949b54948d21
Saeed Soltani <saeed12soltani@gmail.com>
2026-09-25 15:34:41 +0330

    اولین نسخه پروه

---
+++ js/data.js
@@ -0,0 +1,135 @@
+const products = [
+    {
+        id: 1,
+        name: "برگر مخصوص",
+        price: 250000,
+        category: "burger",
+        image: ""
+    },
+    {
+        id: 2,
+        name: "چیز برگر",
+        price: 220000,
+        category: "burger",
+        image: ""
+    },
+    {
+        id: 3,
+        name: "برگر قارچ و پنیر",
+        price: 270000,
+        category: "burger",
+        image: ""
+    },
+    {
+        id: 4,
+        name: "برگر دوبل",
+        price: 320000,
+        category: "burger",
+        image: ""
+    },
+    {
+        id: 5,
+        name: "پیتزا مخصوص",
+        price: 350000,
+        category: "pizza",
+        image: ""
+    },
+    {
+        id: 6,
+        name: "پیتزا پپرونی",
+        price: 330000,
+        category: "pizza",
+        image: ""
+    },
+    {
+        id: 7,
+        name: "پیتزا مرغ و قارچ",
+        price: 320000,
+        category: "pizza",
+        image: ""
+    },
+    {
+        id: 8,
+        name: "پیتزا گوشت و قارچ",
+        price: 340000,
+        category: "pizza",
+        image: ""
+    },
+    {
+        id: 9,
+        name: "فیله سوخاری",
+        price: 280000,
+        category: "fried",
+        image: ""
+    },
+    {
+        id: 10,
+        name: "بال و کتف سوخاری",
+        price: 260000,
+        category: "fried",
+        image: ""
+    },
+    {
+        id: 11,
+        name: "مرغ سوخاری",
+        price: 299000,
+        category: "fried",
+        image: ""
+    },
+    {
+        id: 12,
+        name: "پاستا الفردو",
+        price: 290000,
+        category: "pasta",
+        image: ""
+    },
+    {
+        id: 13,
+        name: "پاستا گوشت",
+        price: 300000,
+        category: "pasta",
+        image: ""
+    },
+    {
+        id: 14,
+        name: "سیب زمینی ویژه ",
+        price: 180000,
+        category: "appetizer",
+        image: ""
+    },
+    {
+        id: 15,
+        name: "قارچ سوخاری",
+        price: 170000,
+        category: "appetizer",
+        image: ""
+    },
+    {
+        id: 16,
+        name: "سالاد سزار",
+        price: 190000,
+        category: "salad",
+        image: ""
+    },
+    {
+        id: 17,
+        name: "سالاد فصل",
+        price: 140000,
+        category: "salad",
+        image: ""
+    },
+    {
+        id: 18,
+        name: "نوشابه",
+        price: 45000,
+        category: "drink",
+        image: ""
+    },
+    {
+        id: 19,
+        name: "موهیتو",
+        price: 120000,
+        category: "drink",
+        image: ""
+    }
+]
